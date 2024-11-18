94383e45f6c165cb338fe253c1baaed4871c70e6
zachseidner1 <zachary.seidner@gmail.com>
2024-11-18 15:07:13 -0500

    Create demo

--- app/src/main/java/com/example/a6starter/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/a6starter/ui/screens/main/MainScreen.kt
@@ -1,9 +1,43 @@
 package com.example.a6starter.ui.screens.main
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 
 @Composable
 fun MainScreen(viewModel: MainScreenViewModel = hiltViewModel()) {
+    val currentViewState = viewModel.mainScreenViewState.collectAsState().value
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Username")
+        TextField(value = currentViewState.username, onValueChange = viewModel::setUsername)
+        Text("Password")
+        TextField(value = currentViewState.password, onValueChange = viewModel::setPassword)
+        Text("Confirm password")
+        TextField(
+            value = currentViewState.confirmPassword,
+            onValueChange = viewModel::setConfirmPassword
+        )
+        currentViewState.errorMessage?.let { error ->
+            Spacer(Modifier.height(16.dp))
+            Text(error, color = Color.Red)
+        }
+
+    }
 }
--- app/src/main/java/com/example/a6starter/ui/screens/main/MainScreenViewModel.kt
+++ app/src/main/java/com/example/a6starter/ui/screens/main/MainScreenViewModel.kt
@@ -1,16 +1,64 @@
 package com.example.a6starter.ui.screens.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.a6starter.data.model.MyRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 data class MainScreenViewState(
-    val property1: Unit = TODO("Specify your Main Screen View State")
+    val username: String = "",
+    val password: String = "",
+    val confirmPassword: String = "",
+    val errorMessage: String? = null,
 )
 
 @HiltViewModel
 class MainScreenViewModel @Inject constructor(
     private val myRepository: MyRepository,
 ) : ViewModel() {
+    private val usernameFlow = MutableStateFlow("")
+    private val passwordFlow = MutableStateFlow("")
+    private val confirmPasswordFlow = MutableStateFlow("")
+
+    val mainScreenViewState: StateFlow<MainScreenViewState> =
+        combine(
+            usernameFlow,
+            passwordFlow,
+            confirmPasswordFlow
+        ) { username, password, confirmPassword ->
+            val baseViewState = MainScreenViewState(username, password, confirmPassword)
+            if (username == password) {
+                return@combine baseViewState.copy(errorMessage = "Username may not be the same as password")
+            }
+            if (password.length < 8) {
+                return@combine baseViewState.copy(errorMessage = "Password does not meet minimum length requirement")
+            }
+            if (password != confirmPassword) {
+                return@combine baseViewState.copy(errorMessage = "Password does not match confirm password")
+            }
+
+            return@combine baseViewState.copy(errorMessage = null)
+        }.stateIn(
+            viewModelScope, SharingStarted.Eagerly,
+            MainScreenViewState("", "", "")
+        )
+
+    fun setUsername(username: String) {
+        usernameFlow.update { username }
+    }
+
+    fun setPassword(password: String) {
+        passwordFlow.update { password }
+    }
+
+    fun setConfirmPassword(confirmPassword: String) {
+        confirmPasswordFlow.update { confirmPassword }
+    }
 }
